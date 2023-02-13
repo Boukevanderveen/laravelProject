8d2a347ab742610e556b06ac5b5e38c6f24fc8dc
webber017 <boukevanderveen@protonmail.com>
2023-02-13 13:54:25 +0100

    verwijderingen

--- resources/views/header.blade.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
-
-<div id="mainHeader">
-    <a id="navBarLogo" href="/">Messenger </a>
-    <a id="navBarUrl1" href="/friendrequests">Vriendschapsverzoeken </a>
-    <a id="navBarUrl2" href="/sendfriendrequest">Stuur vriendschapsverzoek </a>
-    <a id="navBarUrl3" href="/logout">{{\Auth::user()->name}}:         Log uit </a>
-    
-    </div>
